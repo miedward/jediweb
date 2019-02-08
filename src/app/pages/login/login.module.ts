13ab4f446815138d22a88560afd2ce243a988afb
Michael Edwards <mledwards@gmail.com>
2019-02-08 16:48:53 -0500

    working state with login module

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent,
+    LoginRoutingModule
+  ],
   imports: [
     CommonModule
   ]
